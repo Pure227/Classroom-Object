9cdae3172ad9905d206875b40d9ae217717b1b74
Fitywe <96216623+Pure227@users.noreply.github.com>
2023-03-28 20:56:44 +0700

    Classroom Object

---
+++ Classroom Object/Classroom Object/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace Classroom_Object
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxBY = new System.Windows.Forms.TextBox();
+            this.textBoxgpa = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxAvg = new System.Windows.Forms.TextBox();
+            this.textBoxAgeAll = new System.Windows.Forms.TextBox();
+            this.textBoxListName = new System.Windows.Forms.TextBox();
+            this.textBoxMinName = new System.Windows.Forms.TextBox();
+            this.textBoxMaxName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(98, 164);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "AddData";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(98, 65);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxName.TabIndex = 2;
+            // 
+            // textBoxBY
+            // 
+            this.textBoxBY.Location = new System.Drawing.Point(98, 98);
+            this.textBoxBY.Name = "textBoxBY";
+            this.textBoxBY.Size = new System.Drawing.Size(125, 27);
+            this.textBoxBY.TabIndex = 3;
+            // 
+            // textBoxgpa
+            // 
+            this.textBoxgpa.Location = new System.Drawing.Point(98, 131);
+            this.textBoxgpa.Name = "textBoxgpa";
+            this.textBoxgpa.Size = new System.Drawing.Size(125, 27);
+            this.textBoxgpa.TabIndex = 4;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(85, 50);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMax.TabIndex = 5;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(85, 83);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMin.TabIndex = 6;
+            // 
+            // textBoxAvg
+            // 
+            this.textBoxAvg.Location = new System.Drawing.Point(85, 116);
+            this.textBoxAvg.Name = "textBoxAvg";
+            this.textBoxAvg.Size = new System.Drawing.Size(125, 27);
+            this.textBoxAvg.TabIndex = 7;
+            // 
+            // textBoxAgeAll
+            // 
+            this.textBoxAgeAll.Location = new System.Drawing.Point(85, 149);
+            this.textBoxAgeAll.Name = "textBoxAgeAll";
+            this.textBoxAgeAll.Size = new System.Drawing.Size(125, 27);
+            this.textBoxAgeAll.TabIndex = 8;
+            // 
+            // textBoxListName
+            // 
+            this.textBoxListName.Location = new System.Drawing.Point(85, 182);
+            this.textBoxListName.Multiline = true;
+            this.textBoxListName.Name = "textBoxListName";
+            this.textBoxListName.Size = new System.Drawing.Size(125, 95);
+            this.textBoxListName.TabIndex = 9;
+            // 
+            // textBoxMinName
+            // 
+            this.textBoxMinName.Location = new System.Drawing.Point(216, 83);
+            this.textBoxMinName.Name = "textBoxMinName";
+            this.textBoxMinName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMinName.TabIndex = 10;
+            // 
+            // textBoxMaxName
+            // 
+            this.textBoxMaxName.Location = new System.Drawing.Point(216, 50);
+            this.textBoxMaxName.Name = "textBoxMaxName";
+            this.textBoxMaxName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMaxName.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Year of birth";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "GPA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "GPAmax";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "GPAmin";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(17, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 20);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "AvgGPA";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(54, 20);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "AgeAll";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 185);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "ListName";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxMax);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.textBoxMin);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.textBoxAvg);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBoxAgeAll);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxListName);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxMinName);
+            this.groupBox1.Controls.Add(this.textBoxMaxName);
+            this.groupBox1.Location = new System.Drawing.Point(229, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(349, 291);
+            this.groupBox1.TabIndex = 19;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 309);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxgpa);
+            this.Controls.Add(this.textBoxBY);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBoxName;
+        private TextBox textBoxBY;
+        private TextBox textBoxgpa;
+        private TextBox textBoxMax;
+        private TextBox textBoxMin;
+        private TextBox textBoxAvg;
+        private TextBox textBoxAgeAll;
+        private TextBox textBoxListName;
+        private TextBox textBoxMinName;
+        private TextBox textBoxMaxName;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private GroupBox groupBox1;
+    }
+}
---
+++ Classroom Object/Classroom Object/Form1.cs
@@ -0,0 +1,42 @@
+using WinFormsApp1;
+
+namespace Classroom_Object
+{
+    public partial class Form1 : Form
+    {
+        Classroom classroom;
+        public Form1()
+        {
+            InitializeComponent();
+            classroom = new Classroom("OOP");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBoxName.Text;
+            string Byear = textBoxBY.Text;
+            string gpa = textBoxgpa.Text;
+            int iByear = Int32.Parse(Byear);
+            double igpa = double.Parse(gpa);
+
+            Person newPerson = new Person(name, igpa, iByear);
+            this.classroom.AddPerson(newPerson);
+            this.classroom.addGpa();
+            this.textBoxListName.Text = this.classroom.ShowPerons();
+
+
+            this.textBoxAgeAll.Text = this.classroom.showAge().ToString();
+
+            double newMax = this.classroom.showMax();
+            this.textBoxMax.Text = newMax.ToString();
+            this.textBoxMaxName.Text = this.classroom.showMaxName();
+
+            double newMin = this.classroom.showMin();
+            this.textBoxMin.Text = newMin.ToString();
+            this.textBoxMinName.Text = this.classroom.showMinName();
+
+            double newAvg = this.classroom.showAvg();
+            this.textBoxAvg.Text = newAvg.ToString("N2");
+        }
+    }
+}
